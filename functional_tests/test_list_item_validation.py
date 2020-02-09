deb8f93ca0706da8ff7fd7ccef6f0ae916df4bb6
mahagony4 <mahagony4@gmail.com>
2020-02-09 11:52:44 +0100

    clean code for pylint

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,8 +1,8 @@
-from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
-    
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -23,7 +23,7 @@ class ItemValidationTest(FunctionalTest):
 
         # Perversely, she now decides to submit a second blank list item
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
- 
+
         # She receives a similar warning on the list page
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -17,11 +17,11 @@ class ListViewTest(TestCase):
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
-        Item.objects.create(text='itemey 1', list = correct_list)
-        Item.objects.create(text='itemey 2', list = correct_list)
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
         other_list = List.objects.create()
-        Item.objects.create(text='other list item 1', list = other_list)
-        Item.objects.create(text='other list item 2', list = other_list)
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
         response = self.client.get(f'/lists/{correct_list.id}/')
 
@@ -29,18 +29,18 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
-    
+
     def test_passes_correct_list_to_template(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertEqual(response.context['list'], list_)
 
-    def test_can_save_a_POST_request_to_an_existing_list(self):
+    def test_can_save_a_POST_request_to_an_existing_list(self): # pylint: disable=invalid-name
         correct_list = List.objects.create()
 
         self.client.post(
             f'/lists/{correct_list.id}/',
-            data = {'item_text': 'A new item for an existing list'}
+            data={'item_text': 'A new item for an existing list'}
         )
 
         self.assertEqual(Item.objects.count(), 1)
@@ -48,7 +48,7 @@ class ListViewTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_POST_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self): # pylint: disable=invalid-name
         correct_list = List.objects.create()
 
         response = self.client.post(
@@ -66,18 +66,18 @@ class ListViewTest(TestCase):
         self.assertContains(response, expected_error)
 
 class NewListTest(TestCase):
-    def test_can_save_a_POST_request(self):
+    def test_can_save_a_POST_request(self): # pylint: disable=invalid-name
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    def test_redirect_after_POST(self):
+    def test_redirect_after_POST(self): # pylint: disable=invalid-name
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
-    
+
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         response = self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(response.status_code, 200)
