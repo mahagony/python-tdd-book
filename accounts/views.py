3e3a4e3086b673bd0be246faf7471fc4c8ded179
mahagony4 <mahagony4@gmail.com>
2020-02-15 19:37:26 +0100

    send_login_email - manual mock

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+
+def send_login_email(request):
+    send_mail('Your login link for Superlists', 'body', 'noreply@superlists', [request.POST['email']])
+    return redirect('/')
