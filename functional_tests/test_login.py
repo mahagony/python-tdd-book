cf8b0cf588e5c108ebed483416bac6780705fab9
mahagony4 <mahagony4@gmail.com>
2020-02-15 18:46:24 +0100

    login - initiate models User + Token

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,6 @@
+import re
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
 
 from .base import FunctionalTest
 
@@ -43,3 +43,4 @@ class LoginTest(FunctionalTest):
         self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
+        
