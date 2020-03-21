640247185534159fd155daa6989c76a348903ab7
mahagony4 <mahagony4@gmail.com>
2020-03-21 18:05:05 +0100

    actually responding with something

--- lists/tests/test_api.py
+++ lists/tests/test_api.py
@@ -1,6 +1,7 @@
+import json
 from django.test import TestCase
 
-from lists.models import List
+from lists.models import List, Item
 
 class ListAPITest(TestCase):
     base_url = '/api/lists/{}/'
@@ -10,3 +11,18 @@ class ListAPITest(TestCase):
         response = self.client.get(self.base_url.format(list_.id))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['content-type'], 'application/json')
+
+    def test_get_returns_items_for_correct_list(self):
+        other_list = List.objects.create()
+        Item.objects.create(list=other_list, text='item 1')
+        our_list = List.objects.create()
+        item1 = Item.objects.create(list=our_list, text='item 1')
+        item2 = Item.objects.create(list=our_list, text='item 2')
+        response = self.client.get(self.base_url.format(our_list.id))
+        self.assertEqual(
+            json.loads(response.content.decode('utf8')),
+            [
+                {'id': item1.id, 'text': item1.text},
+                {'id': item2.id, 'text': item2.text},
+            ]
+        )
