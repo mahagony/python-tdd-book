955ea8e4bf536afeb605e805bca12f157051cfc0
mahagony4 <mahagony4@gmail.com>
2020-03-22 19:01:21 +0100

    Appendix G. Django-Rest-Framework

--- lists/api.py
+++ lists/api.py
@@ -1,11 +1,8 @@
-import json
-from django.http import HttpResponse
 from rest_framework import routers, serializers, viewsets
 from rest_framework.validators import UniqueTogetherValidator
 
 from lists.models import List, Item
 from lists.forms import (
-    ExistingListItemForm,
     EMPTY_ITEM_ERROR,
     DUPLICATE_ITEM_ERROR,
 )
@@ -30,28 +27,6 @@ class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
         fields = ('id', 'items')
-
-def list(request, list_id): # pylint: disable=redefined-builtin
-    list_ = List.objects.get(id=list_id)
-    if request.method == 'POST':
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse(status=201)
-        else:
-            return HttpResponse(
-                json.dumps({'error': form.errors['text'][0]}),
-                content_type='application/json',
-                status=400,
-            )
-    item_dicts = [
-        {'id': item.id, 'text': item.text}
-        for item in list_.item_set.all()
-    ]
-    return HttpResponse(
-        json.dumps(item_dicts),
-        content_type='application/json'
-    )
 
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
